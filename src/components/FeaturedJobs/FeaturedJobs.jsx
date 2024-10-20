bb50a1d6792b10babc91a9ab28164cbcafb8e782
Alal Hosen <alalhosen1992620120@gmail.com>
2024-10-20 21:33:13 +0600

    six featured card add in Featured Jobs component

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,8 +1,17 @@
+import { useEffect, useState } from "react";
 
 const FeaturedJobs = () => {
+const [jobs, setJobs]=useState([]);
+
+useEffect(()=>{
+fetch('jobs.json')
+.then(res=>res.json())
+.then(data=>setJobs(data));
+}, [])
+
     return (
         <div className="mt-8">
-         <h2 className="text-5xl text-center">Featured Jobs</h2>  
+         <h2 className="text-5xl text-center">Featured Jobs: {jobs.length}</h2>  
          <p className="text-center">Explore thousands of job opportunities with all the information you need. Its your future</p>
         </div>
     );
