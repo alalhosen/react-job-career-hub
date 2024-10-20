0bcc62a1aea8682119b88abbd3fcf15dd839c33d
Alal Hosen <alalhosen1992620120@gmail.com>
2024-10-20 23:36:51 +0600

    use json in Featured component

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,18 +1,26 @@
 import { useEffect, useState } from "react";
+import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
-const [jobs, setJobs]=useState([]);
+    const [jobs, setJobs] = useState([]);
 
-useEffect(()=>{
-fetch('jobs.json')
-.then(res=>res.json())
-.then(data=>setJobs(data));
-}, [])
+    useEffect(() => {
+        fetch('jobs.json')
+            .then(res => res.json())
+            .then(data => setJobs(data));
+    }, [])
 
     return (
-        <div className="mt-8">
-         <h2 className="text-5xl text-center">Featured Jobs: {jobs.length}</h2>  
-         <p className="text-center">Explore thousands of job opportunities with all the information you need. Its your future</p>
+        <div>
+            <div className="mt-8">
+                <h2 className="text-5xl text-center">Featured Jobs: {jobs.length}</h2>
+                <p className="text-center">Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
+            <div>
+                {
+                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                }
+            </div>
         </div>
     );
 };
