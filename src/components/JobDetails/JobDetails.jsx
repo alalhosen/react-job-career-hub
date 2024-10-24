7b357137f8fdce17057d5312906b48c3bedf695c
Alal Hosen <alalhosen1992620120@gmail.com>
2024-10-24 23:53:58 +0600

    css use in Job file

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -3,12 +3,16 @@ import { useLoaderData, useParams } from "react-router-dom";
 const JobDetails = () => {
     const jobs = useLoaderData();
     const { id } = useParams();
-    const idInt=parseInt(id);
-    const job = jobs.find(job => job.id ===idInt);
+    const idInt = parseInt(id);
+    const job = jobs.find(job => job.id === idInt);
     console.log(job);
     return (
         <div>
-            <h2>Job Details of:</h2>
+            <h2>Job Details of:{id}</h2>
+            <div className="grid gap-4 md:grid-cols-4">
+                <div className="border md:col-span-3"></div>
+                <div className="border"></div>
+            </div>
         </div>
     );
 };
