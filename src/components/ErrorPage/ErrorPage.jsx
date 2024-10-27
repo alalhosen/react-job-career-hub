b26e63a550fa60878866bdbb95cd2d2d4cb0715c
Alal Hosen <alalhosen1992620120@gmail.com>
2024-10-27 22:32:36 +0600

    onClick use in Applied Jobs component

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h2>Oops!!!</h2>
+        <div className="text-center m-20">
+            <h2 className="text-2xl">Oops!!!</h2>
             <Link to="/">Go beck to home</Link>
         </div>
     );
