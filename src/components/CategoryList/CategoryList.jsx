9744b3a02c74d9e47fa53ea1b9bf4f0053602a07
Alal Hosen <alalhosen1992620120@gmail.com>
2024-10-29 23:01:08 +0600

    css add in Category List Card

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -4,25 +4,25 @@ const CategoryList = () => {
         <div>
             <div className="mt-8">
                 <h2 className="text-5xl text-center">Job Category List</h2>
-                <p className="text-center">
+                <p className="text-center mt-4">
                     Explore thousands of job opportunities with all the information you need. Its your future
                 </p>
             </div>
-            <div>
+            <div className=" flex space-x-20 mt-3">
                 <div>
-                    <h3>Account & Finance</h3>
+                    <h3 className="text-2xl">Account & Finance</h3>
                     <p>300 Jobs Available</p>
                 </div>
                 <div>
-                    <h3>Create Design</h3>
+                    <h3 className="text-2xl">Create Design</h3>
                     <p>100+ Jobs Available</p>
                 </div>
                 <div>
-                    <h3>Marketing & Sale</h3>
+                    <h3 className="text-2xl">Marketing & Sale</h3>
                     <p>150 Jobs Available</p>
                 </div>
                 <div>
-                    <h3>Engineering Job</h3>
+                    <h3 className="text-2xl">Engineering Job</h3>
                     <p>224 Jobs Available</p>
                 </div>
 
