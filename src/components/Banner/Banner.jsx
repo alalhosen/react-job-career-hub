f0c594dab480d8283749d819bc7b1f54d3550a3a
Alal Hosen <alalhosen1992620120@gmail.com>
2024-10-29 21:06:01 +0600

    change title & discription home to banner component

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,8 +1,11 @@
 
 const Banner = () => {
     return (
-        <div>
+        <div className="text-center">
             <h2 className="text-5xl text-center">Home page Banner</h2>
+            <h3 className="text-3xl">One Step Closer To Your</h3>
+            <h3 className="text-3xl">Dream Job</h3>
+            <p>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
         </div>
     );
 };
